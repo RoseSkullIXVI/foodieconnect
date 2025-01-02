9c36e987c1ff055304d0860ea7e71c1e89f7c5f4
RoseSkullIXVI <athosjnr@gmail.com>
2025-01-02 18:53:30 +0200

    Enhance login component with password visibility toggle and improved input structure

--- app/Login.tsx
+++ app/Login.tsx
@@ -10,25 +10,63 @@ import {
 } from '@/components/ui/button';
 import { B } from "@expo/html-elements";
 import React from "react";
-import { AlertCircleIcon } from "@/components/ui/icon";
-import { Input, InputField } from "@/components/ui/input";
+import { AlertCircleIcon, EyeIcon, EyeOffIcon } from "@/components/ui/icon";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 
 export default function Login() {
+
+  const [showPassword, setShowPassword] = React.useState(false)
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState
+    })
+  }
 
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: '#f7f5ee',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: "#f7f5ee",
+        justifyContent: "center",
+        alignItems: "center",
         paddingHorizontal: 20,
       }}
     >
-     <Heading bold={true} size="3xl" className="text-center mb-2"> Log in</Heading>
-     <Text className="text-center align-bottom mt-2">Enter your details to continue</Text>
-     
-   
+      <Heading bold={true} size="3xl" className="text-center">
+        {" "}
+        Log in
+      </Heading>
+      <Text className="text-center align-bottom mt-2">
+        Enter your details to continue
+      </Text>
+      <FormControl>
+      <FormControlLabel>
+          <FormControlLabelText className="ml-2 mt-2">Enter your e-mail</FormControlLabelText>
+        </FormControlLabel>
+        <Input className="w-full m-2" size="md" variant="rounded">
+          <InputField
+            type="text"
+          />
+        </Input>
+      <FormControlLabel>
+          <FormControlLabelText className="ml-2">Enter your password</FormControlLabelText>
+        </FormControlLabel>
+        <Input className="w-full m-2" size="md" variant="rounded">
+            <InputField type={showPassword ? "text" : "password"} />
+            <InputSlot className="pr-3" onPress={handleState}>
+              <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+            </InputSlot>
+          </Input>
+          
+      </FormControl>
+      <Button
+        size="md"
+        variant="solid"
+        action="custom"
+        className="w-full m-2"
+      >
+        <ButtonText className="text-black">Log in</ButtonText>
+      </Button>
     </View>
   );
 }
